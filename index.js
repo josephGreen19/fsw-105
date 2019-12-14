caade57f99f422ccdd83ec16a087e6adeec7bab7
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2019-12-13 22:33:02 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var shopper = {
+    fName: "Joe",
+    lName: "Green",
+    age: 31,
+    payCash: true,
+    payCard: false,
+    cash: 85,
+    change: 1,
+    totalMoney: function () {
+        return this.cash + " " + this.change;
+    },
+    groceryCart: [ "steak", "milk", "coffee", "bread"]
+};
+console.log(shopper)
