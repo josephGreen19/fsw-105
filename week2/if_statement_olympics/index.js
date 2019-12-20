f158ed73057d6361b4b0b8597ca12238da72d56e
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2019-12-20 10:51:28 -0500

    pauseAndPractice

--- week2/if_statement_olympics/index.js
+++ week2/if_statement_olympics/index.js
@@ -1,43 +1,61 @@
-   // if(5>3) {
-     //    console.log("Is greater than 3")
+    //Prelimiaries:Step1
+     //if(5>3) {
+       //  console.log("Is greater than 3")
     //}
 
-   
+   //Preliminaries:Step2
    // var cat
     //length:3
     //if(3===3){
       //  console.log("Is the length")
-//}
-
+    //}
+   
+    //Preliminaries:lastStep
 //if("cat"==="dog"){
   //  console.log("the same")
 //}else{
-  //  console.log("not the same")
+   // console.log("not the same")
 //}
 
+//bronzeMedalStep1
 //var person = {
-   // name: "Bobby",
-   // age: 12
+  //  name: "Bobby",
+    //age: 12
 //}
 //if(12 >= 18){
-   // console.log("Bobby can go to movies")
+    //console.log("Bobby can go to movies")
 //}else{
   // console.log("Bobby can not go to movies")
 //}
 
+//bronzeMedatStep2
 //if("B"==="B") {
   //console.log("can go to movies")
 //}
 
+//bronzeMedalStep3
 //if("B"==="B" && 12 >= 18){
   //console.log("Bobby can go to movies")
+//}else{
+  //console.log("Bobby can not go to movies")
 //}
 
-//if(1===1){
-  //console.log("strict")
-//}
+//silverMedalStep1
+if(1==="1"){
+  console.log("strict")
+}else{
+  console.log("not strict")
+}
+if(1=="1"){
+  console.log("loose")
+}else{
+  console.log("not equal at all")
+}
 
+//silverMedalStep2
 //if(1 <= 2 && 2===4 ){
   //console.log("yes")
-//          }
+ //}else{
+   //console.log("No")
+ //}
 
