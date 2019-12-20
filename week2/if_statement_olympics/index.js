1c315e0e98576aed3dcd0e812596e66db6a215fa
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2019-12-20 10:06:06 -0500

    first try

---
+++ week2/if_statement_olympics/index.js
@@ -0,0 +1,43 @@
+   // if(5>3) {
+     //    console.log("Is greater than 3")
+    //}
+
+   
+   // var cat
+    //length:3
+    //if(3===3){
+      //  console.log("Is the length")
+//}
+
+//if("cat"==="dog"){
+  //  console.log("the same")
+//}else{
+  //  console.log("not the same")
+//}
+
+//var person = {
+   // name: "Bobby",
+   // age: 12
+//}
+//if(12 >= 18){
+   // console.log("Bobby can go to movies")
+//}else{
+  // console.log("Bobby can not go to movies")
+//}
+
+//if("B"==="B") {
+  //console.log("can go to movies")
+//}
+
+//if("B"==="B" && 12 >= 18){
+  //console.log("Bobby can go to movies")
+//}
+
+//if(1===1){
+  //console.log("strict")
+//}
+
+//if(1 <= 2 && 2===4 ){
+  //console.log("yes")
+//          }
+
