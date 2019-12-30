105f6c05079c8b05b654786c1c30ee386121d551
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2019-12-30 14:25:43 -0500

    pauseAndPractice

--- week2/if_statement_olympics/index.js
+++ week2/if_statement_olympics/index.js
@@ -1,44 +1,44 @@
     //Prelimiaries:Step1
-     //if(5>3) {
-       //  console.log("Is greater than 3")
-    //}
+     if(5>3) {
+         console.log("Is greater than 3")
+    }
 
    //Preliminaries:Step2
-   // var cat
-    //length:3
-    //if(3===3){
-      //  console.log("Is the length")
-    //}
+    var cat
+    length:3
+    if(3===3){
+        console.log("Is the length")
+    }
    
     //Preliminaries:lastStep
-//if("cat"==="dog"){
-  //  console.log("the same")
-//}else{
-   // console.log("not the same")
-//}
+  if("cat"==="dog"){
+    console.log("the same")
+  }else{
+    console.log("not the same")
+  }
 
 //bronzeMedalStep1
-//var person = {
-  //  name: "Bobby",
-    //age: 12
-//}
-//if(12 >= 18){
-    //console.log("Bobby can go to movies")
-//}else{
-  // console.log("Bobby can not go to movies")
-//}
+var person = {
+    name: "Bobby",
+    age: 12
+}
+if(12 >= 18){
+    console.log("Bobby can go to movies")
+}else{
+   console.log("Bobby can not go to movies")
+}
 
 //bronzeMedatStep2
-//if("B"==="B") {
-  //console.log("can go to movies")
-//}
+if("B"==="B") {
+  console.log("can go to movies")
+}
 
 //bronzeMedalStep3
-//if("B"==="B" && 12 >= 18){
-  //console.log("Bobby can go to movies")
-//}else{
-  //console.log("Bobby can not go to movies")
-//}
+if("B"==="B" && 12 >= 18){
+  console.log("Bobby can go to movies")
+}else{
+  console.log("Bobby can not go to movies")
+}
 
 //silverMedalStep1
 if(1==="1"){
@@ -53,9 +53,9 @@ if(1=="1"){
 }
 
 //silverMedalStep2
-//if(1 <= 2 && 2===4 ){
-  //console.log("yes")
- //}else{
-   //console.log("No")
- //}
+if(1 <= 2 && 2===4 ){
+  console.log("yes")
+ }else{
+   console.log("No")
+ }
 
